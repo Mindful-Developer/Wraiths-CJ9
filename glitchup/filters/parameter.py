b7bb1967e8625932033b50cff1a70b72bcb5ba92
akabinds <staticxxd@gmail.com>
2022-07-22 09:10:33 -0500

    `ignore_missing_imports` for `cv2-*` and make mypy happy

--- glitchup/filters/parameter.py
+++ glitchup/filters/parameter.py
@@ -4,30 +4,35 @@
 from __future__ import annotations
 
 from enum import Enum, auto
-from typing import Any, TypeAlias
+from typing import Any, Optional, TypeAlias, Union
 
 __all__ = ("Parameter",)
 
-RANGE: TypeAlias = tuple[int | float, int | float] | None
+# tuple[int | float, int | float] | None (mypy doesn't like this; the type below hopefully is temporary)
+PARAM_RANGE: TypeAlias = Optional[tuple[Union[int, float], Union[int, float]]]
 
 
 class Parameter:
     """Configurable parameter of a filter"""
 
-    class Type(Enum):
+    class ParamType(Enum):
         INT = auto()
         FLOAT = auto()
 
     def __init__(
-        self, type_: Type, name: str, default: int | float, range_: RANGE = None
+        self,
+        param_type: ParamType,
+        name: str,
+        default: int | float,
+        param_range: PARAM_RANGE = None,
     ):
-        self._type = type_
+        self._type = param_type
         self._name = name
         self._default = default
-        self._range = range_
+        self._range = param_range
 
     @property
-    def type(self) -> Type:
+    def param_type(self) -> ParamType:
         """Type of the parameter"""
         return self._type
 
@@ -42,19 +47,22 @@ class Parameter:
         return self._default
 
     @property
-    def range(self) -> RANGE:
+    def param_range(self) -> PARAM_RANGE:
         """Return the range of the parameter"""
         return self._range
 
-    def __dict__(self) -> dict[str, Any]:
+    def to_dict(self) -> dict[str, Any]:
+        """Return a dictionary representation of the parameter"""
         return {
-            "type": self._type.name,
-            "name": self._name,
-            "default": self._default,
-            "range": self._range,
+            "type": self.param_type.name,
+            "name": self.name,
+            "default": self.default,
+            "range": self.param_range,
         }
 
     @staticmethod
     def from_dict(d: dict[str, Any]) -> Parameter:
         """Create a parameter from a dictionary"""
-        return Parameter(Parameter.Type[d["type"]], d["name"], d["default"], d["range"])
+        return Parameter(
+            Parameter.ParamType[d["type"]], d["name"], d["default"], d["range"]
+        )
