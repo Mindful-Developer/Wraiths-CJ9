7809054bae677bdebe7c26585124ba504de43fd7
akabinds <staticxxd@gmail.com>
2022-07-23 09:56:28 -0500

    fastapi server works now

--- glitchup/backend/server.py
+++ glitchup/backend/server.py
@@ -4,12 +4,12 @@ app = FastAPI()
 
 
 @app.on_event("startup")
-async def setup_redis():
+async def setup_redis() -> None:
     """Setup the redis server for the application."""
     pass
 
 
 @app.get("/")
-async def root():
+async def root() -> dict[str, str]:
     """Root route."""
     return {"message": "Hello World"}
