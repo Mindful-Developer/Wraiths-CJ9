16f94bc9c1cb053c3ebcbf3b2d858c3fa3d78034
Grace <872984@pdsb.net>
2022-07-28 10:47:08 -0400

    Changed apply function in numberfilter.py

--- glitchup/filters/numberfilter.py
+++ glitchup/filters/numberfilter.py
@@ -3,6 +3,7 @@ from random import randint as rnt
 from typing import Any
 
 import cv2 as cv
+import numpy as np
 
 from glitchup.filters.image_filter import ImageFilter
 from glitchup.filters.parameter import Parameter
@@ -17,36 +18,34 @@ class NumberFilter(ImageFilter):
 
     def get_params(self) -> list[Parameter]:
         """Return the list of parameters for this filter."""
-        return [
-            Parameter(Parameter.ParamType.INT, "Blur", default=30, param_range=(1, 100))
-        ]
+        return []
 
     def apply(self, img: cv.Mat, params: dict[str, Any]) -> cv.Mat:
         """Apply the filter to the image."""
-        blur_num = params["Blur"].default
-        img = cv.blur(img, (blur_num, blur_num))
-        for x in range(0, img.shape[0], 20):
-            for y in range(0, img.shape[1], 20):
+        img1 = np.zeros((img.shape[0], img.shape[1], 3), dtype="uint8")
+        for x in range(0, img.shape[0], 6):
+            for y in range(0, img.shape[1], 4):
                 b, g, r = img[int(x), int(y)]
                 n = rnt(0, 1)
-                c = (int(b + 50), int(g + 50), int(r + 50))
+                c = (int(b), int(g), int(r))
                 cv.putText(
-                    img,
+                    img1,
                     text=str(n),
                     org=(y, x),
                     fontFace=5,
-                    fontScale=1,
+                    fontScale=0.3,
                     color=c,
-                    thickness=3,
+                    thickness=0,
                 )
-        return img
+        return img1
 
 
 if __name__ == "__main__":
     filter = NumberFilter()
     params = filter.get_params()
     param_dict = {param.name: param for param in params}
-    img = cv.imread(r"cats.jpg")
+    img = cv.imread(r"park.jpg")
+    cv.imshow("Original", img)
     img = filter.apply(img, param_dict)
     cv.imshow("Numbered", img)
     cv.waitKey(0)
