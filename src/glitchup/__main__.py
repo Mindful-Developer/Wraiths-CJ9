a7f1ee225930ed671a547849ada59a8459371eb8
akabinds <staticxxd@gmail.com>
2022-07-31 14:58:01 -0500

    endpoints

--- src/glitchup/__main__.py
+++ src/glitchup/__main__.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
 import asyncio
+import json
 from pathlib import Path
+from typing import Any
 
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI, Form, UploadFile, WebSocket
 from fastapi.responses import FileResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
 from hypercorn.asyncio import serve
@@ -14,21 +16,25 @@ from web.filters.builtin.ghosting import Ghosting  # type: ignore
 from web.filters.builtin.metaldot import MetalDot  # type: ignore
 from web.filters.builtin.number import Number  # type: ignore
 from web.filters.builtin.pixelsort import PixelSort  # type: ignore
-from web.filters.image_filter import ImageFilter  # type: ignore
-from web.filters.parameter import Parameter  # type: ignore
-from web.models import FilterMetadata  # type: ignore
+from web.models import UploadedImage
 from web.worker import Worker, redis_conn, redis_queue  # type: ignore
 
 app = FastAPI()
 BASE_DIR = Path(__file__).resolve().parent
 app.mount("/static", StaticFiles(directory=Path(BASE_DIR, "web/static")), name="static")
-FILTERS = {
-    "981": Dotted,
-    "982": Ghosting,
-    "983": Number,
-    "984": MetalDot,
-    "985": PixelSort,
-}
+
+
+@app.on_event("startup")
+async def setup() -> None:
+    """Add filters to database."""
+    redis_conn.sadd(
+        "filters",
+        str(Ghosting.to_dict()),
+        str(Dotted.to_dict()),
+        str(Number.to_dict()),
+        str(MetalDot.to_dict()),
+        str(PixelSort.to_dict()),
+    )
 
 
 @app.on_event("shutdown")
@@ -48,24 +54,48 @@ async def root() -> FileResponse:
     return FileResponse(f"{BASE_DIR}/web/static/index.html")
 
 
-@app.get("/filters/{filter_id}")
-async def get_filter(filter_id: str) -> JSONResponse:
+@app.get("/filters/filter/{filter_id}")
+async def get_filter(filter_id: int) -> JSONResponse:
     """Get a filter by ID."""
-    if filter_id not in FILTERS:
-        return JSONResponse(
-            status_code=404,
-            content={"message": f"Filter {filter_id} not found."},
-        )
+    filters = redis_conn.smembers("filters")
 
-    filter_class = FILTERS[filter_id]
+    for f in filters:
+        f = json.loads(f.translate(f.maketrans("'()", '"[]')))
+
+        if f["filter_id"] == filter_id:
+            return JSONResponse(
+                content=f,
+                status_code=200,
+            )
+        else:
+            continue
+
     return JSONResponse(
+        content={"error": "Filter not found."},
+        status_code=404,
+    )
+
+
+@app.post("/images/upload")
+async def upload_image(image: UploadFile) -> JSONResponse:
+    """Upload an image to the server."""
+    image_path = Path(BASE_DIR, "web/static/uploads/") / image.filename
+    image_path.write_bytes(await image.read())
+
+    uploaded_image = UploadedImage(path=image_path.as_posix())
+
+    redis_conn.sadd("images", str(uploaded_image.to_dict()))
+
+    return JSONResponse(
+        content=uploaded_image.to_dict(),
         status_code=200,
-        content=filter_class.to_dict(),
     )
 
 
 @app.post("/images/add")
-async def add_image() -> int:
+async def add_image(
+    image_id: int = Form(), filter_id: int = Form(), *params: Any
+) -> int:
     """Upload an image to the server."""
     ...
 
@@ -81,6 +111,7 @@ async def apply_filter_to_image(ws: WebSocket, image_id: int, filter_id: int) ->
 
 @app.websocket("/images/{image_id}")
 async def get_image(image_id: str, ws: WebSocket) -> None:
+    """Get an image by ID."""
     ...
 
 
